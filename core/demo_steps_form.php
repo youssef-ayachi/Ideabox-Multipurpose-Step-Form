b10df2270d92652a41e0f1d1ab5e74e3e33848a4
youssef-ayachi <es.ayachi@gmail.com>
2016-10-29 09:36:28 +0200

    Update demo_steps_form.php

--- core/demo_steps_form.php
+++ core/demo_steps_form.php
@@ -1,6 +1,20 @@
 <?php
 
-	//echo "hello";
-	echo "<font color='#090'>Congragilations! Form post successfly! [From PHP file]</form>"; //or error mesage
+    $to = "info@gmail.com"; /*Your Email*/
+    $date = date ("l, F jS, Y");
+    $time = date ("h:i A");
+        $Email=  $_POST['email'];
+        $Name = $_POST['nom'];
+        $country = $_POST['country'];
+        $Pw = $_POST['pw'];
 
-?>
+        $subject = "Message envoyé du formulaire multi-étapes"; 
+        $msg="
+        Name: $Name
+        Email: $Email
+        Country: $country
+        Courses: $Pw
+        Message sent on: $date, hour: $time.n";
+
+        mail($to, $subject, $msg, "From: $Email")
+?>	
